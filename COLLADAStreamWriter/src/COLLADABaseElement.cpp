22fbefabb3997d8c53a3b21358f828c705b49317
opencollada-sebastian <sebastian@opencollada.org>
2008-07-14 09:58:04 +0000

    Updating file headers

--- COLLADAStreamWriter/src/COLLADABaseElement.cpp
+++ COLLADAStreamWriter/src/COLLADABaseElement.cpp
@@ -1,8 +1,8 @@
 /*
     Copyright (c) 2008 NetAllied Systems GmbH
 
-    Licensed under the MIT Open Source License, for details 
-    please see LICENSE file or the website
+    Licensed under the MIT Open Source License, 
+    for details please see LICENSE file or the website
     http://www.opensource.org/licenses/mit-license.php
 */
 #include "COLLADABaseElement.h"
