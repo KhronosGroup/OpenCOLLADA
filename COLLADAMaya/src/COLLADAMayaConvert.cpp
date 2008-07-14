96e211a2e6dd22d0047f716c21ac9d53cb1ca247
opencollada-sebastian <sebastian@opencollada.org>
2008-07-14 09:52:58 +0000

    Updating file headers

--- COLLADAMaya/src/COLLADAMayaConvert.cpp
+++ COLLADAMaya/src/COLLADAMayaConvert.cpp
@@ -1,8 +1,15 @@
 /*
     Copyright (c) 2008 NetAllied Systems GmbH
+
+	This file is part of COLLADAMaya.
+
+    Portions of the code are:
+    Copyright (c) 2005-2007 Feeling Software Inc.
+    Copyright (c) 2005-2007 Sony Computer Entertainment America
+    Copyright (c) 2004-2005 Alias Systems Corp.
 	
-    Licensed under the MIT Open Source License, for details 
-    please see LICENSE file or the website
+    Licensed under the MIT Open Source License, 
+    for details please see LICENSE file or the website
     http://www.opensource.org/licenses/mit-license.php
 */
 
