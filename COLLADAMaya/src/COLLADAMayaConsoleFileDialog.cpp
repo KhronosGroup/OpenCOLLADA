9680d073f3b2cda6c4cb33bf8b183eac5bb44c25
opencollada-sebastian <sebastian@opencollada.org>
2008-07-23 08:36:07 +0000

    Console application

---
+++ COLLADAMaya/src/COLLADAMayaConsoleFileDialog.cpp
@@ -0,0 +1,151 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+///	\file	FileDialog.cpp
+///	\author	Robert	Bateman
+/// \brief	This just provides some linux and win32 file dialogs. Under Linux it uses gtk2
+///	\note	
+/// 		This sourcefile is provided for free use for non-commercial purposes only. The
+/// 		Software is provided 'as is' and no responsibility can be taken for any damages
+/// 		that may be caused while using it. If you make use of this software within 
+/// 		your code then it would be nice to have a small credit somewhere in your
+/// 		finished product for my hard work :(
+/// 		An e-mail saying you found it useful and possibly even a link to your product
+/// 		would be really nice too ;)
+///
+///			Slip me a couple of quid and i might be kind enough to offer assistance in
+/// 		sorting out your exporters ;) 
+///
+///				(C) 2004 Rob 'give me a job offer' Bateman 
+///
+//////////////////////////////////////////////////////////////////////////////////////////////
+#include "ColladaMayaStableHeaders.h"
+#ifndef WIN32
+#include <gtk/gtk.h>
+#include <string.h>
+#include <assert.h>
+
+extern int   g_argc;
+extern char** g_argv;
+
+char g_FileName[512];
+
+/* Get the selected filename and print it to the console */
+static void file_ok_sel( GtkWidget        *w,
+                         GtkFileSelection *fs )
+{
+	strcpy(g_FileName,gtk_file_selection_get_filename (GTK_FILE_SELECTION (fs)));
+}
+
+int OpenFileDialog(char FileName[])
+{
+    GtkWidget *filew;
+
+	g_FileName[0] = '\0';
+
+    gtk_init (&g_argc, &g_argv);
+
+    /* Create a new file selection widget */
+    filew = gtk_file_selection_new ("File selection");
+
+    g_signal_connect (G_OBJECT (filew), "destroy",
+	              G_CALLBACK (gtk_main_quit), NULL);
+
+    /* Connect the ok_button to file_ok_sel function */
+    g_signal_connect (G_OBJECT (GTK_FILE_SELECTION (filew)->ok_button),
+		      "clicked", G_CALLBACK (file_ok_sel), (gpointer) filew);
+
+    /* Connect the cancel_button to destroy the widget */
+    g_signal_connect_swapped (G_OBJECT (GTK_FILE_SELECTION (filew)->cancel_button),
+							 "clicked",
+							 G_CALLBACK (gtk_widget_destroy),
+			      			 G_OBJECT (filew));
+    /* Connect the cancel_button to destroy the widget */
+    g_signal_connect_swapped (G_OBJECT (GTK_FILE_SELECTION (filew)->ok_button),
+							 "clicked",
+							 G_CALLBACK (gtk_widget_destroy),
+			      			 G_OBJECT (filew));
+
+    gtk_widget_show (filew);
+    gtk_main ();
+
+	if(g_FileName[0] != '\0') {
+		strcpy(FileName,g_FileName);
+		return 1;
+	}
+
+    return 0;
+}
+
+int SaveFileDialog(char FileName[]) {
+	return OpenFileDialog(FileName);
+}
+
+
+#else
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+namespace COLLADAMaya
+{
+
+    int OpenFileDialog(char FileName[])
+    {
+	    char Dir[512];
+	    char fn[512] = {0};
+	    OPENFILENAME ofn;
+	    memset(&ofn,0,sizeof(OPENFILENAME));
+	    ofn.lStructSize = sizeof(OPENFILENAME);
+	    ofn.lpstrFile = fn;
+	    ofn.nMaxFile = 512;
+	    ofn.lpstrTitle = "Open File";
+
+
+	    GetCurrentDirectory(512,Dir);
+	    if( GetOpenFileName(&ofn) )
+	    {
+		    int i;
+		    for (i=0;i<512;++i)
+		    {
+			    if( fn[i] == '\\' )
+				    fn[i] = '/';
+		    }
+		    strcpy(FileName,fn);
+		    SetCurrentDirectory(Dir);
+		    return 1;
+	    }
+	    FileName[0] = '\0';
+	    SetCurrentDirectory(Dir);
+	    return 0;
+    }
+
+    int SaveFileDialog(char FileName[])
+    {
+	    char Dir[512];
+	    char fn[512] = {0};
+	    OPENFILENAME ofn;
+	    memset(&ofn,0,sizeof(OPENFILENAME));
+	    ofn.lStructSize = sizeof(OPENFILENAME);
+	    ofn.lpstrFile = fn;
+	    ofn.nMaxFile = 512;
+	    ofn.lpstrTitle = "Save File";
+	    GetCurrentDirectory(512,Dir);
+	    if( GetSaveFileName(&ofn) )
+	    {
+		    int i;
+		    for (i=0;i<512;++i)
+		    {
+			    if( fn[i] == '\\' )
+				    fn[i] = '/';
+		    }
+		    strcpy(FileName,fn);
+		    SetCurrentDirectory(Dir);
+		    return 1;
+	    }
+	    FileName[0] = '\0';
+	    SetCurrentDirectory(Dir);
+	    return 0;
+    }
+}
+
+#endif
---
+++ COLLADAMaya/src/COLLADAMayaConsoleMain.cpp
@@ -0,0 +1,138 @@
+//--------------------------------------------------------------------------------------------
+/// \file	main.cpp
+/// \author	Rob Bateman
+/// \brief	Probably the simplest maya exporter code possible. This will work on both linux
+/// 		and win32, makes sure that your VisualC++ options has set the correct paths to
+/// 		the maya includes and libraries. When compiling on linux, make sure you have
+/// 		set the correct paths contained within the makefile that is included.
+///
+/// 		When compiling under linux, you will need to have gtk2 installed (the file dialog
+/// 		code uses it. It would be fairly trivial to strip that out however).
+//--------------------------------------------------------------------------------------------
+#include "ColladaMayaStableHeaders.h"
+#include "COLLADAMayaDocumentExporter.h"
+#include "COLLADAMayaExportOptions.h"
+
+#include <maya/MItDependencyNodes.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFileIO.h>
+#include <maya/MLibrary.h>
+
+#include <fstream>
+#include <time.h>
+
+// generic linux(gtk2) & win32 file dialog code
+#include "COLLADAMayaConsoleFileDialog.h"
+#include "COLLADAUtils.h"
+
+// link to some fairly funky maya libs. NOTE: The Image library only became part of maya
+// from version 4.01 onwards. It is not available in version 4.0
+//
+#ifdef WIN32
+	#pragma comment(lib,"Foundation.lib")
+	#pragma comment(lib,"OpenMaya.lib")
+	#pragma comment(lib,"OpenMayaFx.lib")
+	#pragma comment(lib,"Image.lib")
+	#pragma comment(lib,"OpenMayaAnim.lib")
+#endif
+
+// defines
+#define MAX_FILENAME_LEN 512
+
+
+    #ifdef WIN32
+    int main(int argc,char** argv)
+    {
+    #else // hack for linux & gtk
+
+    // gtk needs the command line args for initialisation
+    int   g_argc=0;
+    char** g_argv=0;
+
+    int main(int argc,char** argv)
+    {
+	    g_argc=argc;
+	    g_argv=argv;
+    #endif
+	    char inFileName[MAX_FILENAME_LEN]={0};
+	    char outFileName[MAX_FILENAME_LEN]={0};
+
+	    // both an infile and outfile specified as command line args
+	    if (argc == 3 )
+	    {
+		    strcpy(inFileName,argv[1]);
+		    strcpy(outFileName,argv[2]);
+	    }
+	    else
+
+	    // if only one argument was specified, I'll assume that it is a maya binary
+	    // or ascii file
+	    if (argc == 2 )
+	    {
+		    // got infile from command line args
+		    strcpy(inFileName,argv[1]);
+
+		    // ask for an outfile
+            if(!COLLADAMaya::SaveFileDialog(outFileName)) {
+			    return EXIT_FAILURE;
+		    }
+	    }
+	    else
+ 
+	    // no args specified so pop up a couple fo file dialogs
+	    if (argc == 1 )
+	    {
+		    // get in file name
+            if(!COLLADAMaya::OpenFileDialog(inFileName)) {
+			    return EXIT_FAILURE;
+		    }
+
+		    // get out file name
+            if(!COLLADAMaya::SaveFileDialog(outFileName)) {
+			    return EXIT_FAILURE;
+		    }
+	    }
+	    else {
+		    std::cerr << "[ERROR] Usage\n\tSimpleExporter [infile.mb|infile.ma] [outfile]\n";
+		    return EXIT_FAILURE;
+	    }
+
+	    // initialise the maya library - This basically starts up Maya
+	    MLibrary::initialize(argv[0]);
+
+        // Convert backward to forward slashes
+        for ( size_t i=0; i<MAX_FILENAME_LEN; ++i)
+        {   
+            if ( inFileName[i] == '\\' ) inFileName[i] = '/';
+            if ( outFileName[i] == '\\' ) outFileName[i] = '/';
+        }
+
+	    // open the file that the user specified
+	    if( MS::kSuccess == MFileIO::open(inFileName, NULL, true) )
+	    {
+            MString exportOptions = "relativePaths=false;";
+            COLLADAMaya::ExportOptions::set( exportOptions );
+
+            /** To get the time */
+            clock_t startClock, endClock;
+            startClock = clock();
+
+            bool selectionOnly = false;
+
+            // Actually export the document
+            COLLADAMaya::DocumentExporter documentExporter ( outFileName );
+            documentExporter.exportCurrentScene ( selectionOnly );
+
+            // Display some closing information.
+            endClock = clock();
+            cout << "Time to export into file \"" << outFileName << "\": " << endClock - startClock << " ms" << endl;
+	    }
+	    else {
+		    std::cerr << "[ERROR] Could not open file " << inFileName << std::endl;
+	    }
+
+	    // close down maya
+	    MLibrary::cleanup(0);
+
+	    return 0;
+    }
