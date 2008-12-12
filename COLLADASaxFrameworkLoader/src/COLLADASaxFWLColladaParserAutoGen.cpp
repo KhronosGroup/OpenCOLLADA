1c27ac6ef76893811f48bfc69d5ad9616a310d88
opencollada-sebastian <sebastian@opencollada.org>
2008-12-12 13:00:14 +0000

    add copyright headers

--- COLLADASaxFrameworkLoader/src/COLLADASaxFWLColladaParserAutoGen.cpp
+++ COLLADASaxFrameworkLoader/src/COLLADASaxFWLColladaParserAutoGen.cpp
@@ -1,3 +1,13 @@
+/*
+    Copyright (c) 2008 NetAllied Systems GmbH
+
+    This file is part of COLLADASaxFrameworkLoader.
+
+    Licensed under the MIT Open Source License, 
+    for details please see LICENSE file or the website
+    http://www.opensource.org/licenses/mit-license.php
+*/
+
 #include "COLLADASaxFWLStableHeaders.h"
 #include "GeneratedSaxParserUtils.h"
 #include "COLLADASaxFWLColladaParserAutoGen.h"
