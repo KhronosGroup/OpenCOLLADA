3802edd1d6fecdd058828c73e3cd2225d3a4cbfa
opencollada-sebastian <sebastian@opencollada.org>
2009-01-16 17:18:11 +0000

    import triangles and polygons into maya

---
+++ COLLADASaxFrameworkLoader/include/COLLADASaxFWLPolylist.h
@@ -0,0 +1,97 @@
+/*
+    Copyright (c) 2008 NetAllied Systems GmbH
+
+	This file is part of COLLADASaxFrameworkLoader.
+	
+    Licensed under the MIT Open Source License, 
+    for details please see LICENSE file or the website
+    http://www.opensource.org/licenses/mit-license.php
+*/
+
+#ifndef __COLLADASAXFWL_POLYLIST_H__
+#define __COLLADASAXFWL_POLYLIST_H__
+
+#include "COLLADASaxFWLPrimitiveBase.h"
+#include "COLLADASaxFWLPolyBase.h"
+
+#include "COLLADAFWArray.h"
+
+
+namespace COLLADASaxFWL
+{
+
+    /**
+    * The Polylist element provides the information needed to bind vertex attributes together and  
+    * then organize those vertices into individual polygons. The polygons described in Polylist can  
+    * contain arbitrary numbers of vertices.
+    * Unlike the polygons element, the Polylist element cannot contain polygons with holes.
+    */
+    class Polylist : public MeshPrimitiveInputList
+    {
+
+    private:
+
+        /**
+        * The vcount element contains a array of integers describing the number 
+        * of sides for each polygon described by the Polylist element. 
+        * The vcount element may occur once.
+        */
+        COLLADAFW::UIntValuesArray mVCountArray;
+
+    public:	
+
+        /**
+        * Constructor
+        */
+        Polylist ( MeshLoader* parent ) 
+            : MeshPrimitiveInputList ( parent, COLLADAFW::MeshPrimitive::POLYLIST ) 
+        {}
+
+        /**
+        * Destructor
+        */
+        virtual ~Polylist() {}
+
+        /**
+        * The vcount element contains an array of integers describing the number 
+        * of sides for each polygon  described by the Polylist element. 
+        * The vcount element may occur once.
+        * @param vCountArraySize Parameter to set the size of the array.
+        * @return An integer pointer to the vcount array.
+        */
+        COLLADAFW::UIntValuesArray& getVCountArray () 
+        {
+            return mVCountArray; 
+        }
+
+        /**
+        * The vcount element contains an array of integers describing the number 
+        * of sides for each polygon  described by the Polylist element. 
+        * The vcount element may occur once.
+        * @param vCountArraySize Parameter to set the size of the array.
+        * @return An integer pointer to the vcount array.
+        */
+        const COLLADAFW::UIntValuesArray& getVCountArray () const 
+        {
+            return mVCountArray; 
+        }
+
+        /**
+         * The vcount element contains an array of integers describing the number 
+         * of sides for each polygon  described by the Polylist element. 
+         * The vcount element may occur once.
+         * @param vCountArraySize The size of the array.
+         * @param vCountArray A pointer to the array of vcounts.
+         */
+        void setVCountArray ( const COLLADAFW::UIntValuesArray& vCountArray ) 
+        { 
+            mVCountArray = vCountArray; 
+        }
+
+    };
+
+//    typedef Array<Polylist> PolylistArray;
+
+}
+
+#endif // __COLLADASAXFWL_POLYLIST_H__
