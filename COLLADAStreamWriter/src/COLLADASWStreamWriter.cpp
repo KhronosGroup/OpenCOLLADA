405534d4318e21876169cb83ecf09a03e8d718e3
opencollada-sebastian <sebastian@opencollada.org>
2009-11-11 07:58:30 +0000

    fix bug causes by wrong oder of buffer and flusher deletion

--- COLLADAStreamWriter/src/COLLADASWStreamWriter.cpp
+++ COLLADAStreamWriter/src/COLLADASWStreamWriter.cpp
@@ -86,8 +86,8 @@ namespace COLLADASW
     StreamWriter::~StreamWriter()
     {
         endDocument();
-		delete mBufferFlusher;
 		delete mCharacterBuffer;
+		delete mBufferFlusher;
     }
 
     //---------------------------------------------------------------
